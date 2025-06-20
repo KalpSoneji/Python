403c074eeac27b8976757a277f401bd67633a9c0
Kalp Soneji <kalpsonejicod@gmail.com>
2025-06-20 12:15:08 +0530

    Math Library

---
+++ Math_Library/basic.py
@@ -0,0 +1,31 @@
+# import math
+
+# print(math.sqrt(121)) # square root of 121
+# print(math.pow(2, 5)) # 2 raised to power 5
+# print(math.exp(5)) # e raised to power 5
+
+# print(math.factorial(5)) # factorial of 5
+# print(math.log(10)) # log 10 with base e
+# print(math.log10(10)) # log 10 with base 10
+
+# print(math.floor(4.99)) # returns the last integer
+# print(math.ceil(4.01)) # returns the next integer
+# print(math.fabs(-8.73)) # returns absolute value
+
+# print(math.gcd(4, 8, 12)) # returns HCF of all the numbers
+
+# print(math.degrees(math.pi)) # converts radian into degree, 1 pi = 180 degrees
+
+# print(math.sin(math.pi/2)) # sin 90
+# print(math.cos(0)) # cos 0
+# print(math.tan(math.pi/4)) # tan 45
+
+# print(math.modf(5.75)) # returns after decimal and before decimal seperately
+
+# print(math.copysign(1.0,100)) # takes 1st arg's abs value and sign of 2nd arg
+# print(math.isclose(100+20,120)) # works similar like equal to
+
+# print(math.remainder(10,2)) # returns 1st arg mod 2nd arg
+# print(math.comb(6,3)) # Combinations
+# print(math.perm(5,2)) # Permutations
+# print(math.lcm(12,18, 24)) # lcm of args
